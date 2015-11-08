4abd0d5cf3d24ef78fc93c84f182d030ac995409
Ion D. FIlho <iondrimba@hotmail.com>
2015-11-08 19:54:37 -0200

    code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,13 +61,7 @@ module.exports = function(grunt) {
                     template: require('grunt-template-jasmine-istanbul'),
                     templateOptions: {
                         coverage: 'bin/coverage/coverage.json',
-                        report: 'bin/coverage',
-                        thresholds: {
-                            lines: 75,
-                            statements: 75,
-                            branches: 75,
-                            functions: 90
-                        }
+                        report: 'bin/coverage'
                     }
                 }
             }
