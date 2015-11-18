6fae9c0cc4d0b07dedbfa1082ac004163dcd0f91
Ion D. Filho <iondrimba@hotmail.com>
2015-11-18 15:38:50 -0200

    method cloneObject removed

--- nojquery.js
+++ nojquery.js
@@ -31,19 +31,6 @@
         return result;
     };
 
-    function cloneObject(obj) {
-        if (obj === null || typeof obj !== 'object') {
-            return obj;
-        }
-
-        var temp = obj.constructor(); // give temp the original obj's constructor
-        for (var key in obj) {
-            temp[key] = cloneObject(obj[key]);
-        }
-
-        return temp;
-    }
-
     function NoJQuery() {
 
         return function(selector) {
